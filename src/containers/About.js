9ec17cb66126ca7bf3c19c484956a91a360e748c
morganpstanley <morganpstanley@gmail.com>
2020-08-14 23:42:34 -0600

    add About js/css

---
+++ src/containers/About.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './About.css'
+
+class About extends Component {
+
+
+    render() {
+        return(
+            <div className="about-page">
+                <h1 classname="page-header" > About Me </h1>
+                <p classname="page-text">
+                    My name is Morgan Stanley. 
+                </p>
+            </div>
+        )
+    }
+}
+
+export default About
