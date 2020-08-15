86680808a3f8d04eb3b8cdc469cd2cc5aa0a1329
morganpstanley <morganpstanley@gmail.com>
2020-08-14 23:45:37 -0600

    change App display to grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Menu from './containers/Menu'
+import About from './containers/About'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Menu />
+     <About />
     </div>
   );
 }
