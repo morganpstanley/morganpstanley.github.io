83f351d49742f81bdc9b2117ace35fbd3078036a
morganpstanley <morganpstanley@gmail.com>
2020-08-17 14:53:13 -0600

    activate links in menu sidebar

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react'
 import './Menu.css'
+import {Link} from "react-router-dom";
 
 class Menu extends Component {
 
 
     render() {
         return(
-            <div className="menu">
+        <menu className="menu">
                 <div id="logo">
                     MPS
                 </div>
-                <div className="link" id="menu-about">
-                    About
-                </div>
-                <div className="link" id="menu-portfolio">
-                    Portfolio
-                </div>
-                <div className="link" id="menu-resume">
-                    Resume
-                </div>
-                <div className="link" id="menu-contact-me">
-                    Contact Me
-                </div>
-                <div id="menu-symbol">
-                    <img src="/littlerocket.png" alt="rocket"></img>
-                </div>
+            <div className="link" id="menu-about">
+                <Link to="/">Home</Link>
             </div>
+            <div className="link" id="menu-portfolio">
+                <Link to="/portfolio">Portfolio</Link>
+            </div>
+            <div id="menu-symbol">
+                <img src={process.env.PUBLIC_URL + '/littlerocket.png'} alt="rocket"></img>
+            </div>
+        </menu>
         )
     }
 }
