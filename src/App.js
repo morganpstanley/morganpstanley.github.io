18107c7898b76740cfe2bf3a2613115b3184811e
morganpstanley <morganpstanley@gmail.com>
2020-08-17 14:52:32 -0600

    add ReactRouter and SplitPane to npm, impliment in sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Menu from './containers/Menu'
 import About from './containers/About'
+import Portfolio from './containers/Portfolio'
+import SplitPane from 'react-split-pane'
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Menu />
-     <About />
+        <Router>
+          <SplitPane defaultSize={150}>
+              <Menu/>
+            <div id="page">
+              <Route exact path="/" component={About} />
+              <Route path="/portfolio" component={Portfolio} />
+            </div>
+          </SplitPane>
+        </Router>
     </div>
   );
 }
