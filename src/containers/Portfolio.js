20b928f5020beca7d487c4a27e0ca26e2c0202ce
morganpstanley <morganpstanley@gmail.com>
2020-08-17 14:53:40 -0600

    add Portfolio component

---
+++ src/containers/Portfolio.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './About.css'
+
+class Portfolio extends Component {
+
+
+    render() {
+        return(
+            <div className="portfolio-page">
+                <h1 className="page-header" > Portfolio </h1>
+                <p className="page-text">
+                    Hire me please.
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Portfolio
