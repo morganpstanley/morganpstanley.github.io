245291360515318df7d8900ffe658686f1e49a7b
morganpstanley <morganpstanley@gmail.com>
2020-08-17 14:49:31 -0600

    fix - classname -> className

--- src/containers/About.js
+++ src/containers/About.js
@@ -7,8 +7,8 @@ class About extends Component {
     render() {
         return(
             <div className="about-page">
-                <h1 classname="page-header" > About Me </h1>
-                <p classname="page-text">
+                <h1 className="page-header" > About Me </h1>
+                <p className="page-text">
                     My name is Morgan Stanley. 
                 </p>
             </div>
