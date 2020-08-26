79344468758b008428c3e1160729805911d1ac87
morganpstanley <morganpstanley@gmail.com>
2020-08-25 23:51:19 -0600

    add email to contact

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react'
 
 class Contact extends Component {
 
+    state = {
+        email: "morganpstanley@gmail.com",
+        link: "mailto:morganpstanley@gmail.com"
+    }
+
+    email = () => {
+        return this.state.email
+    }
+
 
     render() {
         return(
             <div className="contact-page">
                 <h1 className="page-header" > Contact </h1>
                 <p className="page-text">
-                    Phone #
+                    <p>If you'd like to contact me, please feel free to email at the address below.</p>
+                    <a href={this.state.link}>{this.state.email} </a>
                 </p>
             </div>
         )
