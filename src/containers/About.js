1d1964d0d18e4b8f83dd09797ffbe08350116c1b
Morgan Stanley <morganpstanley@gmail.com>
2020-10-30 21:57:29 -0600

    modify About to introduce myself

--- src/containers/About.js
+++ src/containers/About.js
@@ -7,9 +7,9 @@ class About extends Component {
     render() {
         return(
             <div className="about-page">
-                <h1 className="page-header" >Under Construction</h1>
+                <h1 className="page-header" >Welcome.</h1>
                 <p className="page-text">
-                    Please check back soon!
+                    My name is Morgan P Stanley. I'm a software developer.
                 </p>
             </div>
         )
