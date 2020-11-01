d5078ca7fe8f8b5aa8025b8aaa4003b21e49a59b
Morgan Stanley <morganpstanley@gmail.com>
2020-10-31 22:57:43 -0600

    import images from assets folder

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -3,6 +3,14 @@ import { Carousel } from "react-responsive-carousel";
 
 import "./Carousel.css"
 import './Portfolio.css'
+import prolift1 from '../assets/prolift-1.png'
+import prolift2 from '../assets/prolift-2.png'
+import prolift3 from '../assets/prolift-3.png'
+import stocker1 from '../assets/stocker-home.png'
+import stocker2 from '../assets/stocker-signup.png'
+import stocker3 from '../assets/stocker-signin.png'
+
+
 
 class Portfolio extends Component {
 
@@ -31,36 +39,36 @@ class Portfolio extends Component {
                     <div className="portfolio-item">
                         <Carousel {...getConfigurableProps()}>
                             <div>
-                                <img id="prolift" class="active" src={process.env.PUBLIC_URL + '/stocker-home.png'} alt="Stocker Portfolio Project"></img>
+                                <img id="prolift" className="active" src={stocker1} alt="Stocker Portfolio Project"></img>
                             </div>
                             <div>
-                                <img id="prolift" src={process.env.PUBLIC_URL + '/stocker-signup.png'} alt="Stocker Portfolio Project"></img>
+                                <img id="prolift" src={stocker2} alt="Stocker Portfolio Project"></img>
                             </div>
                             <div>
-                                <img id="prolift" src={process.env.PUBLIC_URL + '/stocker-signin.png'} alt="Stocker Portfolio Project"></img>
+                                <img id="prolift" src={stocker3} alt="Stocker Portfolio Project"></img>
                             </div>
                         </Carousel>
                     </div>
-                    <div class="description">
-                        <p class="project-name description">Stocker</p>
-                        <p class="description">React + Redux | Ruby on Rails | HTML + CSS</p>
+                    <div className="description">
+                        <p className="project-name description">Stocker</p>
+                        <p className="description">React + Redux | Ruby on Rails | HTML + CSS</p>
                     </div>
                     <div className="portfolio-item">
                         <Carousel {...getConfigurableProps()}>
                             <div>
-                                <img id="prolift" src={process.env.PUBLIC_URL + '/prolift-1.png'} alt="Prolift Portfolio Project"></img>
+                                <img id="prolift" src={prolift1} alt="Prolift Portfolio Project"></img>
                             </div>
                             <div>
-                                <img id="prolift" src={process.env.PUBLIC_URL + '/prolift-2.png'} alt="Prolift Portfolio Project"></img>
+                                <img id="prolift" src={prolift2} alt="Prolift Portfolio Project"></img>
                             </div>
                             <div>
-                            <img id="prolift" src={process.env.PUBLIC_URL + '/prolift-3.png'} alt="Prolift Portfolio Project"></img>
+                                <img id="prolift" src={prolift3} alt="Prolift Portfolio Project"></img>
                             </div>
                         </Carousel>
                     </div>
-                    <div class="description">
-                        <p class="project-name description">Prolift</p>
-                        <p class="description">JavaScript | Ruby on Rails | HTML + CSS</p>
+                    <div className="description">
+                        <p className="project-name description">Prolift</p>
+                        <p className="description">JavaScript | Ruby on Rails | HTML + CSS</p>
                     </div>
                 </div>
             </div>
