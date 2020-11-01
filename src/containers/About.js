f626482239138640c07ebd2dba2d623c91fb0771
Morgan Stanley <morganpstanley@gmail.com>
2020-10-31 22:58:10 -0600

    add background image to home page

--- src/containers/About.js
+++ src/containers/About.js
@@ -7,10 +7,12 @@ class About extends Component {
     render() {
         return(
             <div className="about-page">
+                <div id="about-text">
                 <h1 className="page-header" >Welcome.</h1>
                 <p className="page-text">
                     My name is Morgan P Stanley. I'm a software developer.
                 </p>
+                </div>
             </div>
         )
     }
