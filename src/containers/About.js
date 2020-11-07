5df9a5c16318135185d6f1d78c536028b2752d7f
Morgan Stanley <morganpstanley@gmail.com>
2020-11-07 13:12:17 -0700

    test2

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './About.css'
+import sig from '../assets/white-signature.png'
 
 class About extends Component {
 
@@ -7,11 +8,18 @@ class About extends Component {
     render() {
         return(
             <div className="about-page">
+                <div id="header">
+                    <img src={sig} id="signature" />
+                    <button class="link">About</button>
+                    <button class="link">Portfolio</button>
+                    <button class="link">Contact</button>
+                    <button class="link">Blog</button>
+                </div>
                 <div id="about-text">
-                <h1 className="page-header" >Welcome.</h1>
-                <p className="page-text">
-                    My name is Morgan P Stanley. I'm a software developer.
-                </p>
+                    <h1 className="page-header" >Welcome.</h1>
+                    <p className="page-text">
+                        My name is Morgan P Stanley. I'm a software developer.
+                    </p>
                 </div>
             </div>
         )
