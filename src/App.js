e10bb8076bf2a78692ee4ce6aed1d7fe63c92708
Morgan Stanley <morganpstanley@gmail.com>
2021-05-18 13:18:24 -0600

    change website background and update React from components to hooks

--- src/App.js
+++ src/App.js
@@ -1,83 +1,76 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect} from 'react'
 import './App.css'
 import Contact from "./containers/Contact.js"
 import Portfolio from './containers/Portfolio'
 import sig from './assets/website-signature.png'
 import resume from './assets/software-developer-resume.pdf'
 
-class App extends Component {
+const App = () => {
 
-  state = {
-    aboutButton: true,
-    portfolioButton: false,
-    contactButton: false,
-    mobileMenuOpen: false
-  }
+    useEffect(() => {
+        console.log('𝙇𝙞𝙫𝙚 𝙡𝙤𝙣𝙜 𝙖𝙣𝙙 𝙥𝙧𝙤𝙨𝙥𝙚𝙧. 🖖', 'Thanks for visiting.')
+    },[])
 
-  handleToggle = (e) => {
-    let target = e.target.id
-    if (target === 'aboutButton') {
-      this.setState({
-        aboutButton: true,
-        portfolioButton: false,
-        contactButton: false,
-      })
-  } else if (target === 'portfolioButton') {
-      this.setState({
-        aboutButton: false,
-        portfolioButton: true,
-        contactButton: false,
-      })
-  } else if (target === 'contactButton') {
-      this.setState({
-        aboutButton: false,
-        portfolioButton: false,
-        contactButton: true,
-      })
+    const [about, showAbout] = useState(true)
+    const [portfolio, showPortfolio] = useState(false)
+    const [contact, showContact] = useState(false)
+    const [mobile, showMobile] = useState(false)
+
+    const handleToggle = (e) => {
+        let target = e.target.id
+        if (target === 'about') {
+            showAbout(true)
+            showPortfolio(false)
+            showContact(false)
+        } else if (target === 'portfolio') {
+            showAbout(false)
+            showPortfolio(true)
+            showContact(false)
+        } else if (target === 'contact') {
+            showAbout(false)
+            showPortfolio(false)
+            showContact(true)
+        }
+        if (mobile) {
+            toggleMenu()
+        }
     }
-    if (this.state.mobileMenuOpen) {
-        this.toggleMenu()
+
+    const toggleMenu = () => {
+        showMobile(!mobile)
     }
-  }
 
-  toggleMenu = () => {
-    this.setState({
-        mobileMenuOpen: !this.state.mobileMenuOpen
-    })
-  }
-
-  render() {
-    return(
-      <div className="App">
-        <div id="header">
-            <img src={sig} id="signature" className={this.state.mobileMenuOpen ? "inverted" : null} alt="Morgan Stanley" />
-            <button id="hamburger-icon" className={this.state.mobileMenuOpen ? "showClose" : null}onClick={this.toggleMenu}></button>
-            <nav className={`navbar ${this.state.mobileMenuOpen ? "showNav" : null}`}>
-                <button id="aboutButton" className="link" onClick={this.handleToggle}>About</button>
-                <button id="portfolioButton" className="link" onClick={this.handleToggle}>Portfolio</button>
-                <button id="contactButton" className="link" onClick={this.handleToggle}>Contact</button>
-                <a className="link" href="https://medium.com/@morganpstanley">Blog⤤</a>
-                <a className="link" href={resume} target="_blank" rel="noopener noreferrer">Resume⤤</a>
-            </nav>
+    return (
+        <div className="App">
+            <div id="header">
+                <img src={sig} id="signature" className={mobile ? "inverted" : null} alt="Morgan Stanley" />
+                <button id="hamburger-icon" className={mobile ? "showClose" : null} onClick={toggleMenu}></button>
+                <nav className={`navbar ${mobile ? "showNav" : null}`}>
+                    <button id="about" className="link" onClick={handleToggle}>About</button>
+                    <button id="portfolio" className="link" onClick={handleToggle}>Portfolio</button>
+                    <button id="contact" className="link" onClick={handleToggle}>Contact</button>
+                    <a className="link" href="https://medium.com/@morganpstanley">Blog⤤</a>
+                    <a className="link" href={resume} target="_blank" rel="noopener noreferrer">Resume⤤</a>
+                </nav>
+            </div>
+            <div className={`page ${about && !mobile ? null : "hidden"}`} id="about">
+                <div className="square">
+                    <h1 className="page-header" >Welcome.</h1>
+                    <p className="page-text">
+                        <span>My name is Morgan Stanley. </span> 
+                        <br />
+                        <span>I'm a software developer.</span>
+                    </p>
+                </div>
+            </div>
+            <div className={`page ${contact && !mobile ? null : "hidden"}`} id="contact-area">
+                <Contact />
+            </div>
+            <div className={`page ${portfolio ? null : "hidden"}`} id="portfolio-area">
+                <Portfolio />
+            </div>
         </div>
-        <div className={`page ${this.state.aboutButton && !this.state.mobileMenuOpen ? null : "hidden"}`} id="about">
-          <div className="square">
-            <h1 className="page-header" >Welcome.</h1>
-            <p className="page-text">
-                <span>My name is Morgan Stanley. </span><br />
-                <span>I'm a software developer.</span>
-            </p>
-          </div>
-        </div>
-        <div className={`page ${this.state.contactButton && !this.state.mobileMenuOpen ? null : "hidden"}`} id="contact">
-          <Contact />
-        </div>
-        <div className={`page ${this.state.portfolioButton ? null : "hidden"}`} id="portfolio">
-          <Portfolio />
-        </div>
-      </div>
     )
-  }
 }
 
 export default App;
